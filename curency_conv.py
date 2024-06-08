cb9518a39d0bd3e184dac0369b0344a7a137792b
Simal Agrahari <148173686+simalagrahari223@users.noreply.github.com>
2024-06-08 09:55:53 +0530

    Add files via upload

---
+++ curency_conv.py
@@ -0,0 +1,16 @@
+
+#task -2
+
+with open('test.text') as f:
+	lines = f.readlines()
+
+currencyDict = {}
+for line in lines:
+	parsed = line.split("\t")
+	currencyDict[parsed[0]] = parsed[1]
+
+amount = int(input("Enter the amount:\n"))
+print("Enter the name of the currency you want to convert this amount to? Available Options:\n")
+[print(item) for item in currencyDict.keys()]
+currency = input("Please enter one of these values: \n")
+print(f"{amount} INR is equal to {amount *float(currencyDict[currency])} {currency}")
---
+++ main.py
@@ -0,0 +1,23 @@
+import string
+import random
+if __name__=="__main__":
+    s1 = string.ascii_lowercase
+    #print(s1)
+    s2 = string.ascii_uppercase
+    #print(s2)
+    s3 = string.digits
+    #print(s3)
+    s4 = string.punctuation
+    #print(s4)
+   
+    lgth = int(input("Enter the password length: ")) 
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    #print(s)
+    random.shuffle(s) #shuffling the list
+    #print(s)
+    print("Your password is: ")
+    print("".join(s[0:lgth]))
